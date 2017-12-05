c950750f57c86b524b0fdd7d833de772eac451a9
Izhar Ananiev <izhar_ananiev@hotmail.com>
2017-12-05 11:44:00 +0200

    s

--- Prototype/src/CS/ClientConsole.java
+++ Prototype/src/CS/ClientConsole.java
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
  * interface in order to activate the display() method. Warning: Some of the
  * code here is cloned in ServerConsole
  *
- * @author Fran&ccedil;ois B&eacute;langer
+ * @author Fran&ccedil;aois B&eacute;langer
  * @author Dr Timothy C. Lethbridge
  * @author Dr Robert Lagani&egrave;re
  * @version July 2000
