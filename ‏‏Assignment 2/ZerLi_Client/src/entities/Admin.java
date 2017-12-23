2658d83af2dc763c39ad38701603f30a125df250
Izhar Ananiev <izhar_ananiev@hotmail.com>
2017-12-23 10:23:07 +0200

    Fuck all

--- ‏‏Assignment 2/ZerLi_Client/src/entities/Admin.java
+++ ‏‏Assignment 2/ZerLi_Client/src/entities/Admin.java
@@ -1,8 +1,12 @@
 package entities;
+
+import enums.UserType;
+
 public class Admin extends User {
 
-	public Admin() {
-		super();
+	public Admin(String firstName, String lastName, String userName, String password, UserType permissions) {
+		super(firstName, lastName, userName, password, permissions);
+		// TODO Auto-generated constructor stub
 	}
 
 	private Object AdminID;
--- ‏‏Assignment 2/ZerLi_Client/src/entities/Customer.java
+++ ‏‏Assignment 2/ZerLi_Client/src/entities/Customer.java
@@ -1,8 +1,14 @@
 package entities;
-public class Customer extends User {
 
+import enums.UserType;
+
+public class Customer extends User {
 	private int customerID;
 	private PaymentAccount paymentAccount;
 	private boolean isConnecting;
 
+	public Customer(String firstName, String lastName, String userName, String password, UserType permissions) {
+		super(firstName, lastName, userName, password, permissions);
+		// TODO Auto-generated constructor stub
+	}
 }
