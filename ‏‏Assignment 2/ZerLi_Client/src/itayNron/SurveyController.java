2aca6c706650f67511e38f631c5a64392787a3da
SoftwareBound <benzvi.ron@gmail.com>
2018-01-02 11:30:55 +0200

    ..

--- ‏‏Assignment 2/ZerLi_Client/src/itayNron/SurveyController.java
+++ ‏‏Assignment 2/ZerLi_Client/src/itayNron/SurveyController.java
@@ -1,6 +1,10 @@
 package itayNron;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -8,8 +12,11 @@ import common.Context;
 import controllers.ParentController;
 import entities.CSMessage;
 import entities.Store;
+import entities.StoreWorker;
 import entities.Survey;
 import entities.CSMessage.MessageType;
+import entities.Store.StoreType;
+import entities.Survey.SurveyType;
 import itayNron.interfaces.ISurvey;
 
 public class SurveyController extends ParentController implements ISurvey {
@@ -33,20 +40,65 @@ public class SurveyController extends ParentController implements ISurvey {
 	}
 
 	@Override
-	public void getSurveyByStore(Store store) {
+	public void getSurveyByStore(Store store) throws IOException {
+		myMsgArr.clear();
+		myMsgArr.add(String.format("SELECT survey.*" + "FROM survey" + "WHERE storeID = %d",store.getStoreID()));
+		Context.clientConsole.handleMessageFromClientUI(new CSMessage(MessageType.SELECT, myMsgArr, Survey.class));
 	}
 	
 	@Override
-	public void sendSurvys(ArrayList<Survey> survys) {
+	public void sendSurveys(ArrayList<Survey> Surveys) {
+		String methodName = "setSurveys";
+		Method m = null;
+		try {
+			//a controller asked data, not GUI
+			if(Context.askingCtrl!=null && Context.askingCtrl.size()!=0) {
+				m = Context.askingCtrl.get(0).getClass().getMethod(methodName,ArrayList.class);
+				m.invoke(Context.askingCtrl.get(0), Surveys);
+				Context.askingCtrl.remove(0);
+			}
+			else {
+				m = Context.currentGUI.getClass().getMethod(methodName,ArrayList.class);
+				m.invoke(Context.currentGUI, Surveys);
+			}
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e1) {
+			System.err.println("Couldn't invoke method '"+methodName+"'");
+			e1.printStackTrace();
+		} catch (NoSuchMethodException | SecurityException e2) {
+			System.err.println("No method called '"+methodName+"'");
+			e2.printStackTrace();
+		}
 	}
 
 	@Override
-	public void getSurveyByDates(Date startDate, Date endDate) {
+	public void getSurveyByDates(Date startDate, Date endDate) throws IOException {
+		myMsgArr.clear();
+		myMsgArr.add(
+				"SELECT survey.*" + "FROM survey" + "WHERE (date >= '"+startDate.toString()+"'" + "AND" +  "date <= '"+endDate.toString()+"'"
+				+ "AND" + "type='Answer')");
+		Context.clientConsole.handleMessageFromClientUI(new CSMessage(MessageType.SELECT, myMsgArr, Survey.class));
 	}
 
 	@Override
 	public void handleGet(ArrayList<Object> obj) {
-		
+		ArrayList<Survey> survey = new ArrayList<>();
+		for (int i = 0; i < obj.size(); i += 4)
+				survey.add(parse
+						(
+						BigInteger.valueOf(Long.valueOf((int) obj.get(i))), 
+						
+						(String) obj.get(i + 2), 
+						BigInteger.valueOf((int)obj.get(i+1)),
+						(String) obj.get(i + 3)));
+			 
+		sendSurveys(survey);
+	}
+	
+	
+
+	@Override
+	public Survey parse(BigInteger surveyID, int[] surveyAnswerers, LocalDate date, Store store, SurveyType type) {
+		return new Survey(surveyID,surveyAnswerers,date,store,type);
 		
 	}
 }
--- ‏‏Assignment 2/ZerLi_Client/src/itayNron/interfaces/ISurvey.java
+++ ‏‏Assignment 2/ZerLi_Client/src/itayNron/interfaces/ISurvey.java
@@ -1,10 +1,14 @@
 package itayNron.interfaces;
 
+import java.io.IOException;
+import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
 import entities.Store;
 import entities.Survey;
+import entities.Survey.SurveyType;
 import interfaces.IParent;
 
 public interface ISurvey extends IParent {
@@ -19,15 +23,17 @@ public interface ISurvey extends IParent {
 	 * 
 	 * @param store
 	 */
-	void getSurveyByStore(Store store);
+	void getSurveyByStore(Store store) throws IOException;
 
-	void sendSurvys(ArrayList<Survey> survys);
+	void sendSurveys(ArrayList<Survey> surveys);
 
 	/**
 	 * 
 	 * @param startDate
 	 * @param endDate
 	 */
-	void getSurveyByDates(Date startDate, Date endDate);
+	void getSurveyByDates(Date startDate, Date endDate) throws IOException;
+	
+	public Survey parse(BigInteger surveyID,int[] surveyAnswerers, LocalDate date, Store store, SurveyType type);
 
 }
